7a99a9241dea64b42a2317cf053a7fbfcfce27d2
baggio18-droid <derogerbaggio19@gmail.com>
2022-04-28 04:21:44 +0700

    Components using Arrow Function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Hello = () => {
+  return <p>Hello</p>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,6 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
+ReactDOM.render(<Hello />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
